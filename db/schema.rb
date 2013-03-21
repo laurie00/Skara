38f089aa3d6f21c396a7a96aed0e043682deaafa
Laurie Loydall <laurie.loydall@echobarrier.com>
2013-03-21 06:13:56 +0000

    last night

--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(:version => 20130319114138) do
   add_index "ckeditor_assets", ["assetable_type", "assetable_id"], :name => "idx_ckeditor_assetable"
   add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], :name => "idx_ckeditor_assetable_type"
 
+  create_table "pages", :force => true do |t|
+    t.string   "name"
+    t.text     "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
   create_table "products", :force => true do |t|
     t.string   "name"
     t.text     "description"
